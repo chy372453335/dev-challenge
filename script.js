2920e0003bbcd970eaed290b7fa3063b8ba21f9e
NBdeJames <43949072+chy372453335@users.noreply.github.com>
2021-08-28 20:14:18 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+    var selectGender;
+
+    var url ="https://randomuser.me/api/?results=10";
+    var p ;
+    var moreInfo;
+    var loadMore;
+    
+    
+    fetchInformation(url);
+    
+
+    //radio click function
+    $("input[type='radio']").click(function () {
+
+        $("#volunteers").empty();
+        selectGender=$("input[name='gender']:checked").val();
+        url ="https://randomuser.me/api/?results=10&gender="+selectGender;
+        if (selectGender) {
+            fetchInformation(url);
+        }
+    });
+
+    
+
+
+    //fetch info from url
+    function fetchInformation(url) {
+        fetch(url)
+        .then((response) => (response.json()))
+        .then(function(data){
+
+            //display each volumteer
+            data.results.forEach(person => {         
+                p =`<button id="moreInfo"; type="button"; style="width:100%; text-align:left"
+                onclick="document.getElementById('${person.login.uuid}').style.display='block'">
+
+                <img src="${person.picture.medium}" style="float:right";>
+                ${person.name.title}
+                ${person.name.first}
+                ${person.name.last}
+                <br/>  Emall:${person.email}
+                <br/>  Phone:${person.phone} 
+
+                <!--hidden parts-->
+                <span id="${person.login.uuid}" style="display:none">
+                Username: ${person.login.username}
+                <br/>age: ${person.dob.age}
+                <br/>Address: ${person.location.street.number}
+                ${person.location.street.name}
+                ,${person.location.city}
+                ,${person.location.state}
+                ,${person.location.country}
+                <br/>Postcode:${person.location.postcode}
+                </span>
+
+                </button>`;
+
+                $("#volunteers").append(p);
+            });
+
+            $("#moreInfo").on("click",function(){
+                document.getElementById('moreInfo').style.display='none';
+            });
+            
+            //LoadMore button
+            loadMore = `<button id = "loadmore"; style="background-color:black; color:white; margin:10px"> Load More</button>`;
+            $("#volunteers").append(loadMore);
+            //load 10 more volumteer and delete button after each click
+            $("#loadmore").on("click",function() {
+                fetchInformation(url);
+                $(this).remove();
+            });
+            
+        });
+    }
+
+});
